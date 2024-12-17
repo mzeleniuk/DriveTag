9bac8bf05f92fd6c491f76147c03ca91c14dc7e1
Mykhailo Zeleniuk <zell.xp@gmail.com>
2024-12-17 18:15:44 +0200

    Add QR Code generator

--- src/scripts/form.ts
+++ src/scripts/form.ts
@@ -6,7 +6,31 @@ if (formElement) {
 
         const formData: FormData = new FormData(this as HTMLFormElement);
         const formDataObject: { [p: string]: FormDataEntryValue } = Object.fromEntries(formData.entries());
+        const hasData: boolean = Object.values(formDataObject).some((item: File | number | string): boolean => !!item);
 
-        console.log(formDataObject);
+        if (hasData) {
+            const options: object = {
+                correctLevel: (<any>window).QRCode.CorrectLevel.L,
+                height: 360,
+                text: `${window.location.origin}?token=${btoa(JSON.stringify(formDataObject))}`,
+                title: "Contact Me",
+                titleBackgroundColor: "transparent",
+                titleFont: "normal normal bold 36px Arial",
+                titleHeight: 40,
+                width: 360,
+                onRenderingEnd: function (qrCodeOptions: { _element: { style: { display: string; }; }; }): void {
+                    const submitButton: HTMLButtonElement | null = document.querySelector("button[type='submit']");
+
+                    qrCodeOptions._element.style.display = "flex";
+                    if (submitButton) submitButton.style.display = "none";
+                },
+            };
+
+            new (<any>window).QRCode(document.getElementById("qrcode"), options);
+
+            setTimeout((): void => {
+                window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+            }, 100);
+        }
     });
 }
